0a3d39a57ace0638cd52a44a097d5d15b5f8b4c4
nazmulmilon <milonsarker73@gmail.com>
2022-04-25 18:14:50 +0600

    import travello template and create new app and set up the template in django

--- telusko_django/django_demo/urls.py
+++ telusko_django/django_demo/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('sample_apps.urls')),
+    # path('', include('sample_apps.urls')),
+path('', include('travello.urls')),
     path('admin/', admin.site.urls),
 ]
