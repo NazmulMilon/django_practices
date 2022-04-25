3975299e5ea32077a7e21fb6b9cd23010877996f
nazmulmilon <milonsarker73@gmail.com>
2022-04-25 22:28:31 +0600

    tried to make dynamic contents in templates

--- telusko_django/travello/models.py
+++ telusko_django/travello/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Destination:
+    id: int
+    img: str
+    name: str
+    desc: str
+    price: int
+
+
--- telusko_django/travello/views.py
+++ telusko_django/travello/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 
+
 def index(request):
-    return render(request, 'index.html')
+
+    dest1 = Destination()
+    dest1.name = "Dhaka"
+    dest1.desc = "Capital of BD"
+    dest1.price = 700
+
+    return render(request, 'index.html', {'dest1': dest1})
