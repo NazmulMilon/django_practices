001014c0bc862b46580ee9e13756076797822dc6
nazmulmilon <milonsarker73@gmail.com>
2022-04-26 21:39:02 +0600

    postgres settings done

--- telusko_django/django_demo/settings.py
+++ telusko_django/django_demo/settings.py
@@ -66,8 +66,11 @@ WSGI_APPLICATION = 'django_demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgres',
+        'NAME': 'sample_db',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
     }
 }
 
