fe158bc308e4ebb6999bf7e11f22b2fef864b9ae
nazmulmilon <milonsarker73@gmail.com>
2022-04-27 19:21:04 +0600

    create accounts app and urls

--- telusko_django/django_demo/urls.py
+++ telusko_django/django_demo/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     # path('', include('sample_apps.urls')),
     path('', include('travello.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
