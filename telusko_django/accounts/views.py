94f9692d99aa955a3ecdfdf5c22488df46311b8b
nazmulmilon <milonsarker73@gmail.com>
2022-04-27 20:19:55 +0600

    user registration complete with show messages

--- telusko_django/accounts/views.py
+++ telusko_django/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 
@@ -16,15 +17,18 @@ def register(request):
 
         if password1 == password2:
             if User.objects.filter(username=username).exists():
-                print('Username taken')
+                messages.info(request, 'Username taken')
+                return redirect('register')
+
             elif User.objects.filter(email=email).exists():
-                print('email taken')
+                messages.info(request, 'Email take')
+                return redirect('register')
             else:
 
                 user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name,
                                             last_name=last_name )
                 user.save();
-                print("User Created")
+                messages.success(request, 'User Created')
                 return redirect('/')
         else:
             print("Password Not match")
