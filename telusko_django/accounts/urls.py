9db93d8922bf8ffd9fbddb163e0c0818246e5502
nazmulmilon <milonsarker73@gmail.com>
2022-05-24 19:31:05 +0600

    user login

--- telusko_django/accounts/urls.py
+++ telusko_django/accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('register', views.register, name = 'register'),
-    # path('add', views.add, name='add'),
+    path('login', views.login, name='login'),
+
 ]
--- telusko_django/accounts/views.py
+++ telusko_django/accounts/views.py
@@ -5,6 +5,25 @@ from django.contrib import messages
 # Create your views here.
 
 
+def login(request):
+    if request.method== 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+
+
+
 def register(request):
 
     if request.method == 'POST':
@@ -36,3 +55,5 @@ def register(request):
 
     else:
         return render(request, 'register.html')
+
+
